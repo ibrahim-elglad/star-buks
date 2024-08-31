ef57febc4b2104d57637fe99316bede5ac7a7d05
ibrahim-elglad <ibrahimelglad6@gmail.com>
2024-08-31 20:46:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let img=document.querySelector(".img")
+
+function photos(photo){
+    img.src=photo;
+}
+let cricle = document.querySelector(".cricle")
+
+
+
+function setcolor(color){
+    cricle.style.background=color;
+}
+let main =document.getElementById("main")
+
+
+
+function photo(photo){
+    main.src=photo;
+}
